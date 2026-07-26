8b5402fd492c7aad529391fa50e517def763a662
ahmeedelmasryy <167768459+ahmeedelmasryy@users.noreply.github.com>
2026-07-26 11:11:17 +0300

    ewf

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
 
     <v-app-bar color="primary" elevation="2" v-if="$route.name != 'Login'">
       <v-app-bar-title class="font-weight-bold text-right text-auto" style="direction: rtl">
-        لوحة تحكم المعلم الذكية 🎓
+        مستر هشام عبيد
       </v-app-bar-title>
       <v-btn icon @click="themeStore.toggleTheme()">
         <v-icon>
